a15e258e8c2c6d6066c581c5b0d6f96482bbf90d
ChuChuPL <80755841+ChuChuPL@users.noreply.github.com>
2022-11-21 21:03:54 +0100

    endfunction

--- TileMap/Assets/Scripts/LoadNextLevel.cs
+++ TileMap/Assets/Scripts/LoadNextLevel.cs
@@ -19,7 +19,7 @@ public class LoadNextLevel : MonoBehaviour
     //nur für 2d und bei der Tür muss der trigger angeschaltet werden
     void OnTriggerEnter2D(Collider2D collision){
         GameObject collisionGameObject = collision.gameObject;
-        if(collisionGameObject.name == "Player"){
+        if(collisionGameObject.name == "NikolaWheel"){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
         }
     }
