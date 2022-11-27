f2c7905c3ce1bd48dbf60be46c57b0e2487cc8ba
n-c0de-r <78597469+n-c0de-r@users.noreply.github.com>
2022-11-27 21:31:23 +0100

    Fixing the health bar display
    The Healthbar behaved strangely and was wrongly sized.
    Due to camera problems and sprite overlays.
    This fixes it.
    
    Also the script can now get any energy level and calculate it correctly to fit any slider values.
    On top made the bar change colors to fit the palette.

--- TileMap/Assets/Scripts/HealthBar.cs
+++ TileMap/Assets/Scripts/HealthBar.cs
@@ -7,10 +7,22 @@ using UnityEngine.UI;
     
 public class HealthBar : MonoBehaviour
 {
+    // Get relevant components
     public Slider slider;
-    
+    public Image fill = GameObject.Find("Fill").GetComponent<Image>();
 
-    public void SetHealth(float health) { 
-        slider.value = health;
+    private Color[] colors = {new Color32(195,  65, 70, 255),   // Red
+                              new Color32(231, 140, 91, 255),   // Orange
+                              new Color32(247, 213, 84, 255),   // Yellow
+                              new Color32(141, 162, 78, 255) }; // Green
+
+    public void SetHealth(float health) {
+        // Calculate relevant values for color index and slider position
+        int colorIndex = (int) (health / (100.0 / colors.Length));
+        int rounded = Mathf.RoundToInt(health / slider.maxValue);
+
+        // Apply new values to the components
+        slider.value = rounded;
+        fill.color = colors[colorIndex];
     }
 }
